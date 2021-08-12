e567f51219a7865c7c298f1d7979e5978b503dca
aikalmine <aikalmine@gmail.com>
2021-08-12 22:05:55 +0500

    template is ready

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -4,9 +4,8 @@ function Blogs(){
             <div class="section-inner shadow-sm rounded">
                 <h2 class="heading">Latest Blog Posts</h2>
                 <p>You can use Sascha Depold's <a href="https://github.com/sdepold/jquery-rss" target="_blank">Vanilla RSS plugin</a> to pull in your blog post feeds.</p>
-                <div id="rss-feeds" class="content">
-                </div>
-            </div>
+                <a rel="noreferrer" class="btn btn-cta-secondary btn-follow" href="https://twitter.com/3rdwave_themes" target="_blank"><i class="fab fa-twitter"></i> Follow on twitter</a>
+            </div>  
         </aside>
     );
 }
